52d977bcca31a289e6db29c4b43e326612903026
TrungRegit <trungnv@regit-technology.com>
2023-11-27 13:50:46 +0700

    edit users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->integer("id")->primary();
             $table->string('username')->nullable(false);
-            $table->string('email',50)->nullable(false)->unique();
-            $table->timestamp('email_verified_at')->nullable(false);
-            $table->string('password',255)->nullable(false);
+            $table->string('email', 255)->nullable(false)->unique();
+            $table->string('link_avatar', 255)->nullable(false);
+            $table->string('password', 255)->nullable(false);
             $table->tinyInteger('role')->nullable(false);
-            $table->string('token',255)->nullable(false);
-            $table->string('link_avatar',255)->nullable(false);
+            $table->timestamp('email_verified_at')->nullable(false);
+            $table->string('token_veryfy_email', 255)->nullable(false);
+            $table->timestamp('token_reset_password_create_at');
+            $table->string('token_reset_password', 255)->unique();
         });
     }
 
