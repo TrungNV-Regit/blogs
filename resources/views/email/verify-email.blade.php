636abe2449ba63a42919a14c42bc49d351631f01
TrungRegit <trungnv@regit-technology.com>
2023-12-01 01:25:31 +0700

    PR 20233011

--- resources/views/email/verify-email.blade.php
+++ resources/views/email/verify-email.blade.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html>
-
 <head>
 	<title>Verify Email</title>
 </head>
-
 <body>
-	
+
 	<h2><a href="{{ $content }}">Verify</a></h2>
 	<p>Hello,</p>
 	<p>This is a verify mail</p>
 	<strong>Thanks.</strong>
-	
+
 </body>
 </html>
