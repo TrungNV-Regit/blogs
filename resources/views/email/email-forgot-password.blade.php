fcb7107b99116fb6ebdcfbf36bce647764c01459
TrungRegit <trungnv@regit-technology.com>
2023-12-01 01:37:33 +0700

    PR 20233011

--- resources/views/email/email-forgot-password.blade.php
+++ resources/views/email/email-forgot-password.blade.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Fotgot Password</title>
 </head>
-
 <body>
-
     <h2>Hello, this is your new password : {{$content}}</h2>
     <strong>Thanks.</strong>
-
 </body>
 </html>
