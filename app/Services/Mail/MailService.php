1c506b941afda8cfe106d27115bb9da74bf18a54
TrungRegit <trungnv@regit-technology.com>
2023-11-30 13:22:57 +0700

    done view sign in

--- app/Services/Mail/MailService.php
+++ app/Services/Mail/MailService.php
@@ -2,23 +2,20 @@
 
 namespace App\Services\Mail;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class MailService
 {
     public function verifyEmail(string $token): bool
     {
-        $user = DB::table("users")
-            ->where("token_verify_email", $token)
+        $user = User::where("token_verify_email", $token)
             ->where('email_verified_at', null)
             ->first();
         if ($user) {
-            DB::table("users")
-                ->where("token_verify_email", $token)
-                ->update(["email_verified_at" => now()]);
+            $user->email_verified_at = now();
+            $user->save();
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -1,3 +1,65 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RT Blogs | Sign in</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/svn-gilroy" rel="stylesheet">
+    @vite(['resources/scss/app.scss'])
+</head>
+
+<body>
+    <div class="logo">
+        <div>
+            <a href="./index.html">
+                <img src="{{Vite::asset('resources/images/logo.png')}}" alt="Logo">
+                <span>RT-Blogs</span>
+            </a>
+        </div>
+    </div>
+    <div class="sign-up">
+        <form action="{{route('auth.sign-in')}}" method="post">
+            {{ csrf_field() }}
+
+            <h4>Sign up</h4>
+
+            <label for="eamil">Username or email <span>*</span></label>
+            <input type="text" name="email">
+
+            @if($errors->has('email'))
+            <p>{{ $errors->first('email') }}</p>
+            @endif
+
+            <label for="username">Password <span>*</span></label>
+            <input type="password" name="password">
+
+            @if($errors->has('password'))
+            <p>{{ $errors->first('password') }}</p>
+            @endif
+
+            <div class='option'>
+                <div>
+                    <input type="checkbox" name="remember" class="checkbox">
+                    <span>Remember password</span>
+                </div>
+
+                <div>
+                    <a>Forgot your password?</a>
+                </div>
+            </div>
+
+            <div>
+                <button type="submit">Login</button>
+            </div>
+
+            <div>
+                <a href="{{route('auth.sign-up')}}">Don’t have an account? Sign up here</a>
+            </div>
+
+        </form>
+    </div>
+</body>
+
+</html>
