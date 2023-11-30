0ffd3cc5aedf8abed06e47a4324d7cecb0a53e71
TrungRegit <trungnv@regit-technology.com>
2023-11-30 13:22:57 +0700

    done view sign in

--- app/Http/Controllers/auth/SignInController.php
+++ app/Http/Controllers/auth/SignInController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SignInRequest;
 
 class SignInController extends Controller
 {
     public function signInForm()
     {
         return view('auth.sign-in');;
+    }
+
+    public function signIn(SignInRequest $request)
+    {
+        echo $request;
+        // $request->validated();
+        // return ;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
    Route::get('/sign-up', [SignUpController::class, 'signUpForm'])->name('sign-up');
    Route::post('/sign-up', [SignUpController::class, 'signUp'])->name('sign-up');
    Route::get('/sign-in', [SignInController::class, 'signInForm'])->name('sign-in');
+   Route::post('/sign-in', [SignInController::class, 'signIn'])->name('sign-in');
    Route::get('/verify-email',[VerificationController::class,'verifyEmail'])->name('verify-email');
 });
 
