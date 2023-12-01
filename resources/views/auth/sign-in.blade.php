6abc8b5fb1af01332308e9ebb00f15f1f87264cb
TrungRegit <trungnv@regit-technology.com>
2023-12-01 14:31:12 +0700

    edit view

--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -4,43 +4,48 @@
 
 @section('content')
 
-@include('layouts.logo')
-
 <div class="sign-up">
-    <form action="{{route('auth.sign-in')}}" method="post">
-        {{ csrf_field() }}
+    <div>
 
-        <h4>Sign up</h4>
+        @include('layouts.logo')
 
-        <label for="eamil">Username or email <span>*</span></label>
-        <input type="text" name="email">
+        <div class="content">
+            <form action="{{route('auth.sign-in')}}" method="post">
+                {{ csrf_field() }}
 
-        @if($errors->has('email'))
-        <p>{{ $errors->first('email') }}</p>
-        @endif
+                <h4>Sign up</h4>
 
-        <label for="username">Password <span>*</span></label>
-        <input type="password" name="password">
+                <label for="eamil">Username or email <span>*</span></label>
+                <input type="text" name="email">
 
-        @if($errors->has('password'))
-        <p>{{ $errors->first('password') }}</p>
-        @endif
+                @if($errors->has('email'))
+                <p>{{ $errors->first('email') }}</p>
+                @endif
 
-        <div class='option'>
-            <div>
-                <input type="checkbox" name="remember" class="checkbox">
-                <span>Remember password</span>
-            </div>
-            <div>
-                <a>Forgot your password?</a>
-            </div>
+                <label for="username">Password <span>*</span></label>
+                <input type="password" name="password">
+
+                @if($errors->has('password'))
+                <p>{{ $errors->first('password') }}</p>
+                @endif
+
+                <div class='option'>
+                    <div>
+                        <input type="checkbox" name="remember" class="checkbox">
+                        <span>Remember password</span>
+                    </div>
+                    <div>
+                        <a>Forgot your password?</a>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+                <div>
+                    <a href="{{route('auth.sign-up')}}">Don’t have an account? Sign up here</a>
+                </div>
+            </form>
         </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
-        <div>
-            <a href="{{route('auth.sign-up')}}">Don’t have an account? Sign up here</a>
-        </div>
-    </form>
+    </div>
 </div>
 @endsection
--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -3,55 +3,60 @@
 @section('title', 'Sign up')
 
 @section('content')
-@if (session('success'))
-<div class='notification'>
-    <span> {{ session('success') }}</span>
-</div>
-@endif
 
-@include('layouts.logo')
-
-<div class="sign-up">
-    <form action="{{ route('auth.sign-up') }}" method="post">
-        {{ csrf_field() }}
-
-        <h4>Sign up</h4>
-
-        <label for="username">Username <span>*</span></label>
-        <input type="text" name="username" value="{{ old('username') }}">
-
-        @if($errors->has('username'))
-        <p>{{ $errors->first('username') }}</p>
-        @endif
-
-        <label for="username">Email <span>*</span></label>
-        <input type="email" name="email" value="{{ old('email') }}">
-
-        @if($errors->has('email'))
-        <p>{{ $errors->first('email') }}</p>
-        @endif
-
-        <label for="username">Password <span>*</span></label>
-        <input type="password" name="password">
-
-        @if($errors->has('password'))
-        <p>{{ $errors->first('password') }}</p>
-        @endif
-
-        <label for="password">Password confirm <span>*</span></label>
-        <input type="password" name="password_confirmation">
-
-        @if($errors->has('passwordConfirm'))
-        <p>{{ $errors->first('passwordConfirm') }}</p>
-        @endif
-
-        <div>
-            <button type="submit">Sign up</button>
+<div class='sign-up'>
+    <div>
+        @if (session('success'))
+        <div class='notification'>
+            <span> {{ session('success') }}</span>
         </div>
-        <div>
-            <a href="{{ route('auth.sign-in') }}">Already have an account? Login</a>
-        </div>
+        @endif
 
-    </form>
+        @include('layouts.logo')
+
+        <div class="content">
+            <form action="{{ route('auth.sign-up') }}" method="post">
+                {{ csrf_field() }}
+
+                <h4>Sign up</h4>
+
+                <label for="username">Username <span>*</span></label>
+                <input type="text" name="username" id='username' value="{{ old('username') }}">
+
+                @if($errors->has('username'))
+                <p>{{ $errors->first('username') }}</p>
+                @endif
+
+                <label for="username">Email <span>*</span></label>
+                <input type="email" name="email" id=email value="{{ old('email') }}">
+
+                @if($errors->has('email'))
+                <p>{{ $errors->first('email') }}</p>
+                @endif
+
+                <label for="username">Password <span>*</span></label>
+                <input type="password" id='password' name="password">
+
+                @if($errors->has('password'))
+                <p>{{ $errors->first('password') }}</p>
+                @endif
+
+                <label for="password">Password confirm <span>*</span></label>
+                <input type="password" name="password_confirmation" id='password_confirmation'>
+
+                @if($errors->has('password'))
+                <p>{{ $errors->first('password') }}</p>
+                @endif
+
+                <div>
+                    <button type="submit">Sign up</button>
+                </div>
+                <div>
+                    <a href="{{ route('auth.sign-in') }}">Already have an account? Login</a>
+                </div>
+
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
