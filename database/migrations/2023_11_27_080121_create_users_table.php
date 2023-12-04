67d0a5c8128f350f9d4ae5fbe3eb51ea56857fbb
TrungRegit <trungnv@regit-technology.com>
2023-12-04 08:45:22 +0700

    handle exception

--- database/migrations/2023_11_27_080121_create_users_table.php
+++ database/migrations/2023_11_27_080121_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username', 255);
+            $table->string('username', 255)->unique();
             $table->string('email', 255)->unique();
             $table->string('link_avatar', 255);
             $table->string('password', 255);
