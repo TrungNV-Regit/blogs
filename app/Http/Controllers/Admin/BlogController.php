a429a2ddead8d72c12fce1c2e595a00f3458768b
TrungRegit <trungnv@regit-technology.com>
2023-12-08 14:32:16 +0700

    edit view/admin/blogs

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use App\Services\Admin\BlogService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
@@ -17,7 +18,11 @@ class BlogController extends Controller
     public function index(int $status): View
     {
         $data = $this->blogService->index($status);
-        return view('admin.blogs', compact('data'));
+        $statuses = [
+            Blog::STATUS_ACTIVE => __('message.active'),
+            Blog::STATUS_PENDING => __('message.pending'),
+        ];
+        return view('admin.blogs', compact('data', 'statuses'));
     }
 
     public function changeStatus(int $id): RedirectResponse
--- resources/views/admin/blogs.blade.php
+++ resources/views/admin/blogs.blade.php
@@ -8,11 +8,6 @@
 
 @section('class', 'header-static')
 
-@php
-    use App\Models\User;
-    use App\Models\Blog;
-@endphp
-
 <div class="blogs">
     <div class='breadcrumb'>
         <a href="{{ route('/') }}">{{ __('message.home') }} > <span>{{ __('message.list_blog') }}</span> </a>
@@ -20,15 +15,12 @@
     <div class="d-flex">
         <h4 class='text-danger'>{{ __('message.choose_blog_type') }}</h4>
         <select onchange="window.location.href = this.value;">
-            <option disabled> {{ __('message.choose_blog_type') }}</option>
-            <option value="{{ route('blog.index', ['status' => Blog::STATUS_ACTIVE]) }}"
-                {{ request('status') == Blog::STATUS_ACTIVE ? 'selected' : '' }}>
-                {{ __('message.active') }}
-            </option>
-            <option value="{{ route('blog.index', ['status' => Blog::STATUS_PENDING]) }}"
-                {{ request('status') == Blog::STATUS_PENDING ? 'selected' : '' }}>
-                {{ __('message.pending') }}
-            </option>
+            @foreach ($statuses as $key => $status)
+                <option value="{{ route('blog.index', ['status' => $key]) }}"
+                    {{ request('status') == $key ? 'selected' : '' }}>
+                    {{ $status }}
+                </option>
+            @endforeach
         </select>
     </div>
 
