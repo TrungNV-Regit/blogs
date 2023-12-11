a6bf085ad0d9ddaf627bb8fdd1cf3f602d5ce00a
TrungRegit <trungnv@regit-technology.com>
2023-12-11 14:33:39 +0700

    edit by comment

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,6 @@ Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
       Route::get('/create', [UserBlogController::class, 'create'])->name('create');
       Route::post('/create', [UserBlogController::class, 'store'])->name('create');
       Route::get('/my-blogs', [UserBlogController::class, 'myBlogs'])->name('my-blogs');
-      Route::get('/my-blogs', [UserBlogController::class, 'myBlogs'])->name('my-blogs');
    });
 
    Route::middleware(['auth'])->group(function () {
