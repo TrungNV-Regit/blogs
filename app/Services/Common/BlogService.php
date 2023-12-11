301ddbdd9fc021e1eb4b462e7f91ef984202e4a1
TrungRegit <trungnv@regit-technology.com>
2023-12-11 16:17:03 +0700

    delete blog

--- app/Services/Common/BlogService.php
+++ app/Services/Common/BlogService.php
@@ -27,13 +27,13 @@ class BlogService
         try {
             $data = $request->only('title', 'content', 'category_id', 'image');
 
-            if ($request->has('checkDeleteImage') ) {
+            if ( $request->has('checkDeleteImage') ) {
                 $this->imageService->deleteImage($blog->link_image);
                 $data['link_image'] = null;
             }
 
-            if ($request->hasFile('image') ) {
-                if ($blog->link_image ) {
+            if ( $request->hasFile('image') ) {
+                if ( $blog->link_image ) {
                     $this->imageService->deleteImage($blog->link_image);
                 }
                 $data['link_image'] = $this->imageService->uploadImage($data['image']);
@@ -51,7 +51,7 @@ class BlogService
             if ( $blog->link_image ) {
                 $this->imageService->deleteImage($blog->link_image);
             }
-            return  $blog->delete();
+            return $blog->delete();
         } catch (Exception $ex) {
             throw new Exception($ex->getMessage());
         }
